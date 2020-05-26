7836ce118d9b6372c9b22f5e1dcd70ecf676362b
Robert Rößger <r.roessger@vet-concept.de>
2020-05-26 19:43:33 +0200

    impl business logic

--- src/Program.cs
+++ src/Program.cs
@@ -4,9 +4,30 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Using shell conventions for return typ: 0 = ok, else = error
+        /// </summary>
+        /// <param name="args">not used</param>
+        /// <returns>0 = ok, else = error</returns>
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input number a: ");
+            var strA = Console.ReadLine();
+
+            Console.Write("Input number b: ");
+            var strB = Console.ReadLine();
+
+            int a, b;
+            if (!int.TryParse(strA, out a) || !int.TryParse(strB, out b))
+            {
+                Console.Error.WriteLine("Malformed input");
+                return -1;
+            }
+
+            int result = a + b;
+
+            Console.WriteLine($"a + b = {result}");
+            return 0;
         }
     }
 }

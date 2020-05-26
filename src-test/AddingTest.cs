8c6257689e52e498569614a3f020e6bffeeb91a1
Robert Rößger <r.roessger@vet-concept.de>
2020-05-26 20:20:28 +0200

    impl unit-test

---
+++ src-test/AddingTest.cs
@@ -0,0 +1,18 @@
+using AddTwoNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddTwoNumbersTests
+{
+    [TestClass]
+    public class AddingTest
+    {
+        [TestMethod]
+        public void TestAddingTwoNumbers()
+        {
+            var fixture = new { a = 5, b = 4, expected = 9 };
+
+            var actual = Program.AddNumbers(fixture.a, fixture.b);
+            Assert.AreEqual(fixture.expected, actual);
+        }
+    }
+}
